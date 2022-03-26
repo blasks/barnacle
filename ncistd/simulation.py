fixed dependencies, masking in heatmap vis, noise in simulations

ncistd/simulation.py
@@ -2,6 +2,8 @@
 cluster data based on several different models"""
 import numpy as np
 import itertools
+import scipy.sparse as sparse
+import scipy.stats as stats
 from tensorly import check_random_state
 from tensorly.tenalg import outer
 from tensorly.cp_tensor import CPTensor
@@ -101,6 +103,7 @@ class SimulationTensor(CPTensor):
         
     def to_tensor(self, 
                   noise_level=0, 
+                  sparse_noise=False, 
                   noise_distribution=None, 
                   random_state=None):
         """Generate optionally noisey data tensor from factorized CP tensor.
@@ -109,6 +112,9 @@ class SimulationTensor(CPTensor):
         ----------
         noise_level : float, optional
             Scale factor for the noise tensor, relative to the l2 norms.
+        sparse_noise : bool
+            If True, will set all positions in the noise matrix that correspond
+            to sparse positions in the signal matrix to zero. Default is False.
         noise_distribution : scipy.stats.rv_continuous, optional
             Parameterized continuous distribution to generate the noise tensor.
             This parameter cannot be None if noise_level > 0.
@@ -124,16 +130,15 @@ class SimulationTensor(CPTensor):
         data = super().to_tensor()
         if noise_level == 0:
             return data
-        elif noise_distribution is None:
-            raise ValueError('For noise_level > 0, you must pass a ' 
-                             'parameterized scipy.stats.rv_continuous '
-                             'distribution from which the noise tensor '
-                             'will be drawn.')
         else:
+            if noise_distribution is None:
+                noise_distribution = stats.norm()
             # initialize random generator
             rns = check_random_state(random_state)
             # add noise to data tensor
             noise = noise_distribution.rvs(size=self.shape, random_state=rns)
+            if sparse_noise:
+                noise = noise * (data != 0)
             noise /= np.linalg.norm(noise)
             noise *= noise_level * self.norm()
             data += noise
@@ -296,7 +301,7 @@ def simulated_sparse_tensor(shape,
             data_rvs=factor_rvs_list[i]
         )
         factors.append(factor.A)
-    return CPTensor((weights, factors))
+    return SimulationTensor((weights, factors))
 
 # ##########
 # # Function to generate original Block Model

ncistd/utils.py
@@ -1,7 +1,9 @@
 """The utils module contains tools for visualizing and manipulating tensors"""
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import tensorly as tl
+from matplotlib import pyplot as plt
 from plotly.express import scatter_3d
 from sklearn.metrics import jaccard_score
 from tensorly import check_random_state
@@ -84,6 +86,7 @@ def visualize_3d_tensor(tensor,
 ##########
 def plot_factors_heatmap(factors, 
                          ratios=False, 
+                         mask_thold=None, 
                          reference_factors=None, 
                          figsize=None, 
                          heatmap_kwargs=None):
@@ -97,6 +100,10 @@ def plot_factors_heatmap(factors,
         True = heights of plots are proportional to dimensions of factors
         False = heights of plots are identical
         list of ints = manual assignment of height ratios
+    mask_thold : tuple of floats
+        Interval (inclusive) between which all values will be masked out of heatmaps. 
+        Example: 
+            (0, 0) = only values that are exactly zero will be masked.
     reference_factors : list of numpy.ndarray, optional
         A second set of baseline factors to be plotted. Sizes and shapes are 
         assumed to be the same as in `factors`. If not None, `reference_factors`
@@ -133,28 +140,36 @@ def plot_factors_heatmap(factors,
         gridspec_kw={'height_ratios': ratios}
     )
     for i in range(rows):
-        # plot reference
-        if not i:
-            cbar = True
+        # get factor mask
+        if mask_thold is not None:
+            fact_mask = np.logical_and((factors[i] >= mask_thold[0]), 
+                                        (factors[i] <= mask_thold[1]))
         else:
-            cbar = False
+            fact_mask = None
+        # plot reference
         if reference_factors is not None:
+            # get reference mask
+            if mask_thold is not None:
+                ref_mask = np.logical_and((reference_factors[i] >= mask_thold[0]), 
+                                          (reference_factors[i] <= mask_thold[1]))
+            else:
+                ref_mask = None
             sns.heatmap(
                 reference_factors[i], 
-                mask=(reference_factors[i]==0), 
-                **heatmap_params, 
+                mask=ref_mask, 
+                **heatmap_kwargs, 
                 ax=ax[i][0]
             )
             sns.heatmap(
                 factors[i], 
-                mask=(factors[i]==0), 
+                mask=fact_mask, 
                 **heatmap_kwargs, 
                 ax=ax[i][1]
             )
         else:
             sns.heatmap(
                 factors[i], 
-                mask=(factors[i]==0), 
+                mask=fact_mask, 
                 **heatmap_kwargs, 
                 ax=ax[i]
             )